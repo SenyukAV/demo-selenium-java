8c7b739e1bfed4c01d1e183a48d971625912ba77
Aliaksandr Sianiuk <avsenyuk@gmail.com>
2024-10-27 22:11:10 +0300

    add LoginXpath

---
+++ src/test/java/com/netflix/LoginXpath.java
@@ -0,0 +1,11 @@
+package com.netflix;
+
+public class LoginXpath {
+    public final static String INPUT_EMAIL_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div/input";
+    public final static String INPUT_PASSWORD_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div/input";
+    public final static String ENTER_BUTTON_XPATH = "/html/body/div[1]/div/div/div/div/form/button[1]";
+    public final static String TEXT_ERROR_NO_EMAIL_XPATH = "/html/body/div[1]/div/div/div/div/form/div[1]/div/div[2]";
+    public final static String TEXT_ERROR_NO_PASSWORD_XPATH = "/html/body/div[1]/div/div/div/div/form/div[2]/div/div[2]";
+    public final static String TEXT_ERROR_INPUT_PASSWORD_EMAIL_XPATH = "/html/body/div[1]/div/div/div[2]/div/header/div/div/div";
+
+}
