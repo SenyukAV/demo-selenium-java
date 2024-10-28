33d767229c21836ad37d7e936618b740803a706e
Aliaksandr Sianiuk <avsenyuk@gmail.com>
2024-10-28 19:46:40 +0300

    add implicitlyWait

--- src/test/java/com/netflix/LoginTest.java
+++ src/test/java/com/netflix/LoginTest.java
@@ -5,11 +5,14 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class LoginTest {
     @Test
     public void testNoInputData() {
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://www.netflix.com/login");
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.toClickButtonEnter();
         Assert.assertEquals(LoginMessage.TEXT_ERROR_NO_EMAIL, loginPage.toGetMessageError(LoginXpath.TEXT_ERROR_NO_EMAIL_XPATH));
@@ -19,6 +22,7 @@ public class LoginTest {
     @Test
     public void testNoInputEmail() {
         WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         webDriver.get("https://www.netflix.com/login");
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.toEnterPassword("qwerty");
@@ -29,6 +33,7 @@ public class LoginTest {
     @Test
     public void testNoInputPassword() {
         WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         webDriver.get("https://www.netflix.com/login");
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.toClickButtonEnter();
@@ -39,6 +44,7 @@ public class LoginTest {
     @Test
     public void testInputEmailPassword() {
         WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         webDriver.get("https://www.netflix.com/login");
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.toEnterEmail(LoginPage.email);
